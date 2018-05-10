61d0b77400453fca76176262b4ea8f8e6bd2800a
Jhonny Afonso <jafonso@teravisiontech.com>
2018-05-10 16:58:23 -0400

    KnpUniversity Symfony4 Course

---
+++ src/Controller/ArticleAdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\ArticleRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new")
+     */
+    public function new(ArticleRepository $articleRepository)
+    {
+        return $articleRepository->saveArticle();
+    }
+}
